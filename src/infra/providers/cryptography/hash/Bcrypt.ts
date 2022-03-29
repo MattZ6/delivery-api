559e7ba59df07a477698f6b5ce0b4be178503255
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-03-29 17:30:45 -0300

    feat(HashProvider): add bcrypt implementation

---
+++ src/infra/providers/cryptography/hash/Bcrypt.ts
@@ -0,0 +1,19 @@
+import { hash, compare } from 'bcrypt';
+
+import { ICompareHashProvider, IGenerateHashProvider } from '@application/protocols/providers/cryptography/hash';
+
+export class BcryptHashProvider implements IGenerateHashProvider, ICompareHashProvider {
+  constructor(private readonly salt: number) {}
+
+  async hash(data: IGenerateHashProvider.Input): Promise<string> {
+    const { value } = data;
+
+    return hash(value, this.salt);
+  }
+
+  async compare(data: ICompareHashProvider.Input): Promise<boolean> {
+    const { value, hashed_value } = data;
+
+    return compare(value, hashed_value);
+  }
+}
