db69d192971b4365dafb29f882a304f4dc7d00f7
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-03-31 15:14:04 -0300

    feat(RefreshClientAccessToken): add resource route

--- src/main/routes/client.routes.ts
+++ src/main/routes/client.routes.ts
@@ -3,10 +3,15 @@ import { Router } from 'express';
 import { adaptRoute } from '@main/adapters/express/routeAdapter';
 import { makeAuthenticateClientController } from '@main/factories/controllers/client/Authenticate';
 import { makeCreateClientController } from '@main/factories/controllers/client/Create';
+import { makeRefreshClientAccessTokenController } from '@main/factories/controllers/client/RefreshAccessToken';
 
 const routes = Router();
 
 routes.post('/sign/in', adaptRoute(makeAuthenticateClientController()));
 routes.post('/sign/up', adaptRoute(makeCreateClientController()));
+routes.post(
+  '/auth/refresh',
+  adaptRoute(makeRefreshClientAccessTokenController())
+);
 
 export default routes;
