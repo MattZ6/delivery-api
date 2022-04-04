fbde1b670d64fa0ac7afa5fb4e2d2c24469e2df7
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-04-04 17:00:28 -0300

    feat(StartDelivery): add resource route

--- src/main/routes/delivery.routes.ts
+++ src/main/routes/delivery.routes.ts
@@ -4,6 +4,7 @@ import { adaptMiddleware } from '@main/adapters/express/middlewareAdapter';
 import { adaptRoute } from '@main/adapters/express/routeAdapter';
 import { makeCreateDeliveryController } from '@main/factories/controllers/delivery/Create';
 import { makeFindAllAvailableDeliveriesController } from '@main/factories/controllers/delivery/FindAllAvailable';
+import { makeStartDeliveryController } from '@main/factories/controllers/delivery/Start';
 import { makeClientAuthenticationMiddleware } from '@main/factories/middlewares/client/Authentication';
 import { makeDeliverymanAuthenticationMiddleware } from '@main/factories/middlewares/deliveryman/Authentication';
 
@@ -19,6 +20,12 @@ routes.get(
   '/available',
   adaptMiddleware(makeDeliverymanAuthenticationMiddleware()),
   adaptRoute(makeFindAllAvailableDeliveriesController())
+);
+
+routes.post(
+  '/:delivery_id/start',
+  adaptMiddleware(makeDeliverymanAuthenticationMiddleware()),
+  adaptRoute(makeStartDeliveryController())
 );
 
 export default routes;
