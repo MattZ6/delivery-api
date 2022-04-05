451bd53d99b75cc69aa474a9bdcc39f42b896558
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-04-05 10:00:56 -0300

    refactor(http-responses): move to their own folders

---
+++ src/presentation/helpers/http/responses/4xx/index.ts
@@ -0,0 +1,6 @@
+export * from './badRequest';
+export * from './conflict';
+export * from './forbidden';
+export * from './notFound';
+export * from './unauthorized';
+export * from './unprocessableEntity';
