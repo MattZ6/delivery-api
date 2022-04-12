f3a322a0b61c4a880b3feb011c91e2fa830ae645
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-04-11 21:04:45 -0300

    chore: update docker config and server initialization

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,21 +1,10 @@
-import { config } from 'dotenv';
-
-function setupEnv() {
-  config();
-}
+import { app } from './config/app';
+import { apiConfig } from './config/env/api';
 
 async function startServer() {
-  const { apiConfig } = await import('@main/config/env/api');
-  const { app } = await import('@main/config/app');
-
   app.listen(apiConfig.PORT, () => {
     console.log(`🍌 App is running at ${apiConfig.URL}:${apiConfig.PORT}`);
   });
 }
 
-function initialize() {
-  setupEnv();
-  startServer();
-}
-
-initialize();
+startServer();
